4e933816d9b7b5255b061682f17b49b8f988fd59
AlexPlynin <44000243+AlexPlynin@users.noreply.github.com>
2018-10-09 20:26:12 +0300

    Add files via upload

---
+++ task12/task1208/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static void print(String s,int a){
+
+
+    }
+    public static void print(String s,Integer a){
+
+
+    }
+    public static void print(int s,String a){
+
+
+    }
+    public static void print(String s,int a,char v){
+
+
+    }
+    public static void print(String s,int a,int c){
+
+
+    }
+
+    //Напишите тут ваши методы
+}
